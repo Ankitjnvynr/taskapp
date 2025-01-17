52cc92c1fc4062edf7d49123ab7b377aa69ddddd
ankitjnvynr <ankitbkana@outlook.com>
2025-01-17 13:37:29 +0530

    first commit

--- assest/_db.php
+++ assest/_db.php
@@ -1,15 +1,23 @@
 <?php
-$servername = "sql107.ezyro.com";
-$username = "ezyro_33857799";
-$password ="Ankit@";
-$db = "ezyro_33857799_mytask";
+// Include Composer's autoload file
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Load environment variables from the .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Fetch the database credentials from the environment variables
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$db = getenv('DB_NAME');
 
 // Create connection
-$conn = new mysqli($servername, $username, $password,$db);
+$conn = new mysqli($servername, $username, $password, $db);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 // echo "Connected successfully";
 ?>
