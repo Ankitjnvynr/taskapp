724dd83adbe4a92d959c6bcfd3f24b80413159ee
ankitjnvynr <ankitbkana@outlook.com>
2025-01-17 13:47:34 +0530

    updated dot env error

--- assest/_db.php
+++ assest/_db.php
@@ -1,23 +1,22 @@
 <?php
-// Include Composer's autoload file
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// Load environment variables from the .env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+use Dotenv\Dotenv;
+
+// Load .env variables
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-// Fetch the database credentials from the environment variables
-$servername = getenv('DB_HOST');
-$username = getenv('DB_USERNAME');
-$password = getenv('DB_PASSWORD');
-$db = getenv('DB_NAME');
+// Example of accessing an environment variable
+$dbHost = $_ENV['DB_HOST'];
+$dbUser = $_ENV['DB_USER'];
+$dbPassword = $_ENV['DB_PASSWORD'];
+$dbName = $_ENV['DB_NAME'];
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $db);
+// Database connection
+$conn = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// echo "Connected successfully";
 ?>
